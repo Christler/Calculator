8b0313fab6d19f42d89f2e5b924bd9e70b33a515
Chris Ear <christear85@gmail.com>
2021-12-15 17:01:03 -0700

    got it mostly working

--- app.js
+++ app.js
@@ -1,3 +1,53 @@
+let a = 0
+let b = 0
+let operator = ""
+let value = "0"
+const output = document.querySelector(".output")
+
+const numbers = document.querySelectorAll(".number")
+numbers.forEach(number => number.addEventListener("click", () => {
+    if(number.innerHTML === "."){
+        if(!value.includes(".")){
+            value += number.innerHTML
+        }
+    }else{
+        value += number.innerHTML 
+        value = Number(value).toString()
+    }
+    displayOutput()
+}))
+
+const clearBtn = document.querySelector(".clear")
+clearBtn.addEventListener("click", clear)
+
+const operators = document.querySelectorAll(".operator")
+operators.forEach(o => o.addEventListener("click", () => {
+    b = Number(value)
+    a = operate(operator, a, b)
+    operator = o.innerHTML
+    value = ""
+    output.innerHTML = a.toString()
+}))
+
+const equals = document.querySelector(".equals")
+equals.addEventListener("click", () => {
+    b = Number(value)
+    value = operate(operator, a, b)
+    a = 0
+    displayOutput()
+})
+
+function displayOutput(){
+    output.innerHTML = value
+}
+
+function clear(){
+    a = 0
+    operator = ""
+    value = "0"
+    output.innerHTML = value
+}
+
 function add(a, b){
     return a + b
 }
@@ -12,8 +62,8 @@ function multiply(a, b){
 
 function divide(a, b){
     if (b === 0) {
-        //do something
-    } else{
+        output.innerHTML = "Can't divide by 0" 
+    }else{
         return a / b
     }
 }
@@ -27,11 +77,14 @@ function operate(operator, a, b){
         case "-":
          sum = subtract(a, b)
          break 
-        case "*":
+        case "x":
          sum = multiply(a, b)
          break 
-        case "/":
+        case "/":   
          sum = divide(a, b)
+         break  
+        default:
+         sum = add(a, b)  
          break    
     }
 
